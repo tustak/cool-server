be973f5b1712b861d0e00bb2ae48b5203ffe5372
andresmechali <andresmechali@gmail.com>
2017-11-21 10:30:03 +0100

    add cors access headers

--- src/start.js
+++ src/start.js
@@ -169,7 +169,12 @@ export const start = async () => {
 
     const app = express()
 
-    app.use(cors())
+    //app.use(cors())
+    app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
 
     /*app.use('/graphql', bodyParser.json(), graphqlExpress({
       schema: schema,
