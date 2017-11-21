649e140bff1a7765dc1ed2190aabf5c88651040f
andresmechali <andresmechali@gmail.com>
2017-11-21 16:52:47 +0100

    try fix cors 4

--- dist/start.js
+++ dist/start.js
@@ -504,7 +504,7 @@ var start = exports.start = function _callee() {
 
           app.use(function (req, res, next) {
             res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
             if (req.method === 'OPTIONS') {
               res.sendStatus(200);
             } else {
--- src/start.js
+++ src/start.js
@@ -179,7 +179,7 @@ export const start = async () => {
     //app.use(cors())
     app.use(function(req, res, next) {
       res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
       if (req.method === 'OPTIONS') {
         res.sendStatus(200);
       } else {
